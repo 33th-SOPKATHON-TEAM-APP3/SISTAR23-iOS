d151e23513245fc73f1d5c120be4ccce9e89b2ba
Hyowon Jeon <hyowon612@gmail.com>
2023-11-26 01:42:12 +0900

    [ADD] #5 - URLConstant 추가

--- 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Base/URLConstant.swift
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Base/URLConstant.swift
@@ -11,15 +11,20 @@ enum URLConstant {
     
     // MARK: - Base URL
     
-    /// static let baseURL = Config.baseURL
+    static let baseURL = ""
+    
+    // MARK: - User
+    
+    // 유저 회원가입 (POST)
+    static let userURL = "/user/signup"
     
     // MARK: - Question
     
+    // 오늘의 질문 조회 (GET), 내가 답변한 질문 리스트 조회 (GET)
+    static let questionURL = "/question"
     
-    // MARK: - Archiving
+    // MARK: - Answer
     
-    
-    // MARK: - ArchivingDetail
-
-    /// static let tablingURL = baseURL + "/orders"
+    // 오늘의 답변 저장 (POST), 나의 답변 조회 (GET), 랜덤 답변 조회 (GET)
+    static let answerURL = "/answer"
 }
