0c28d90e006f07997c833c2900786714f7dfbd49
Hyowon Jeon <hyowon612@gmail.com>
2023-11-26 02:27:55 +0900

    [ADD] #5 - GenericResponse 수정

--- 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Base/GenericResponse.swift
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Base/GenericResponse.swift
@@ -8,13 +8,18 @@
 import Foundation
 
 struct GenericResponse<T: Codable>: Codable {
-    let code: String?
-    let message: String?
-    let status: Int
-    let data: T?
+    var code: Int
+    var message: String?
+    var data: T?
     
     enum CodingKeys: String, CodingKey {
-        case code, message, status
-        case data = "result"
+        case code, message, data
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        code = (try? values.decode(Int.self, forKey: .code)) ?? 0
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode(T.self, forKey: .data)) ?? nil
     }
 }
