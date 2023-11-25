23af776f5c7eb8d19e26b7097b12d449c129089a
0seoYun <yys020819@gmail.com>
2023-11-26 04:19:50 +0900

    [Add] #8 - 이미지 추가

---
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Presentation/ArchivingDetail/View/ArchivingDetailAnswerView.swift
@@ -0,0 +1,20 @@
+//
+//  ArchivingDetailAnswerView.swift
+//  33th-SOPKATHON-iOS-TEAM3
+//
+//  Created by 윤영서 on 11/26/23.
+//
+
+import UIKit
+
+class ArchivingDetailAnswerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
