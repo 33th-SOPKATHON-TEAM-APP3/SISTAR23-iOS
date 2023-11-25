5b7bb8caace9e818ae354792864300712cda434d
0seoYun <yys020819@gmail.com>
2023-11-26 04:20:30 +0900

    [Feat] #8 - 뷰로 나누어 구현

--- 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Presentation/ArchivingDetail/View/ArchivingDetailAnswerView.swift
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Presentation/ArchivingDetail/View/ArchivingDetailAnswerView.swift
@@ -7,14 +7,82 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class ArchivingDetailAnswerView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Properties
+    
+    public var titleLabel = UILabel()
+    public let pinkHorzLineView = UIView()
+    public var contentLabel = UILabel()
+    
+    // MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyles()
+        setupViews()
+        setupConstraints()
+        setupProperties()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+// MARK: - Private Method
+
+private extension ArchivingDetailAnswerView {
+    
+    func setupStyles() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 15
+    }
+    
+    func setupViews() {
+        addSubviews(titleLabel, pinkHorzLineView, contentLabel)
+    }
+    
+    func setupConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(21)
+            $0.centerX.equalToSuperview()
+        }
+        
+        pinkHorzLineView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(titleLabel.snp.leading).offset(-16)
+            $0.trailing.equalTo(titleLabel.snp.trailing).offset(16)
+            $0.height.equalTo(1)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(pinkHorzLineView.snp.bottom).offset(14)
+            $0.leading.trailing.equalToSuperview().inset(37)
+            $0.bottom.equalToSuperview().inset(22)
+        }
+    }
+    
+    func setupProperties() {
+        titleLabel.do {
+            $0.font = .title3
+            $0.textColor = .black
+        }
+        
+        pinkHorzLineView.do {
+            $0.backgroundColor = .pink
+        }
+        
+        contentLabel.do {
+            $0.font = .body1
+            $0.textColor = .black
+            $0.numberOfLines = 0
+        }
+    }
 }
