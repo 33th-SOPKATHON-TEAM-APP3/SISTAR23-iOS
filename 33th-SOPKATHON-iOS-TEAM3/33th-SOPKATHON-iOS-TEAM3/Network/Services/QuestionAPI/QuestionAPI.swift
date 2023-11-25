9bd3527696235427e0aea20801098946ba1feeba
Hyowon Jeon <hyowon612@gmail.com>
2023-11-26 02:30:42 +0900

    [ADD] #5 - QuestionAPI/QuestionService 추가

--- 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Services/QuestionAPI/QuestionAPI.swift
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Services/QuestionAPI/QuestionAPI.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+import Moya
+
+final class QuestionAPI {
+    
+    static let shared = QuestionAPI()
+    var questionProvider = MoyaProvider<QuestionService>(plugins: [MoyaLoggerPlugin()])
+    
+    public init() { }
+    
+
+    func getTodayQuestion(completion: @escaping (NetworkResult<Any>) -> Void) {
+        questionProvider.request(.getTodayQuestion) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeTodayQuestionStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getQuestionList(userId: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        questionProvider.request(.getQuestionList(userId: userId)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeQuestionListStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func judgeTodayQuestionStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<TodayQuestionModel>.self, from: data)
+        else {
+            return .pathErr
+        }
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "success")
+        case 400..<500:
+            return .requestErr(decodedData.code)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func judgeQuestionListStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<QuestionListModel>.self, from: data)
+        else {
+            return .pathErr
+        }
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "success")
+        case 400..<500:
+            return .requestErr(decodedData.code)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
--- 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Services/QuestionAPI/QuestionService.swift
+++ 33th-SOPKATHON-iOS-TEAM3/33th-SOPKATHON-iOS-TEAM3/Network/Services/QuestionAPI/QuestionService.swift
@@ -1,8 +1,55 @@
 //
-//  ExampleSurvice.swift
+//  QuestionService.swift
 //  33th-SOPKATHON-iOS-TEAM3
 //
 //  Created by 민 on 11/25/23.
 //
 
 import Foundation
+import Moya
+
+enum QuestionService {
+    case getTodayQuestion
+    case getQuestionList(userId: Int)
+}
+
+extension QuestionService: TargetType {
+    
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getTodayQuestion, .getQuestionList:
+            return URLConstant.questionURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getTodayQuestion, .getQuestionList:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getTodayQuestion:
+            return .requestPlain
+        case .getQuestionList(let userId):
+            return .requestParameters(parameters: [
+                "userId": userId
+            ], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .getTodayQuestion, .getQuestionList:
+            return [
+                "Content-Type": "application/json"
+            ]
+        }
+    }
+}
